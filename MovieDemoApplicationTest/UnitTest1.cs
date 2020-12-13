788af7f41261c3a8581c18e7b20c4652d3395ae7
meghaingle <inglemegha@gmail.com>
2020-12-14 01:17:15 +0530

    Added Test Method

--- MovieDemoApplicationTest/UnitTest1.cs
+++ MovieDemoApplicationTest/UnitTest1.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieDemoApplication.Controllers;
+using System.Web.Mvc;
 
 namespace MovieDemoApplicationTest
 {
@@ -9,6 +10,26 @@ namespace MovieDemoApplicationTest
         [TestMethod]
         public void IndexView()
         {
+            string Search_Data = "Avatar";
+            string Filter_Value = "Avatar";
+            int? Page_No = 1;
+            MovieController movieController = new MovieController();
+            // Act
+            ViewResult result = movieController.Index(Search_Data, Filter_Value, Page_No) as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DetailsView()
+        {
+            string Search_Data = "Avatar";
+           
+            MovieController movieController = new MovieController();
+            // Act
+            ViewResult result = movieController.Details(Search_Data) as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
         }
     }
 }
